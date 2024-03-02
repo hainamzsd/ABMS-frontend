666c01b69d94b4d6d6ce9ad6d093dd42733c1396
Nguyễn Ngọc Tùng Lâm <85636591+lam1201nhe@users.noreply.github.com>
2024-03-02 13:40:43 +0700

    add account management of cmb

--- app/web/Admin/index.tsx
+++ app/web/Admin/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useAuth } from '../context/AuthContext';
+import { Redirect } from 'expo-router';
+import { Text, View } from 'react-native';
 
-const index = () => {
+const Dashboard = () => {
+  const { user, isLoading } = useAuth();
+
+  if (!user) {
+    return <Redirect href="/web/Admin/accountManagement/accountManagement" />;
+  }
+
   return (
-    <div>index</div>
+    <View>
+      <Text>Hello world</Text>
+    </View>
   )
 }
 
-export default index
+export default Dashboard
---
+++ components/layout/web/navbar.tsx
@@ -0,0 +1,105 @@
+import {
+    Image,
+    Text,
+    TouchableOpacity,
+    View,
+    Pressable,
+} from "react-native";
+import { Link, Stack, useNavigation, usePathname } from "expo-router";
+import { Menu as PaperMenu } from "react-native-paper";
+import React, { useEffect, useState } from "react";
+
+const Navbar = () => {
+    const [visible, setVisible] = React.useState(false);
+    const openMenu = () => setVisible(true);
+    const closeMenu = () => setVisible(false);
+    const navigation = [
+        { name: "Trang chính", href: "/web" },
+        {
+            name: "Quản lý tài khoản",
+            href: "/screens/CMB/accountManagement/accountManagement",
+        },
+        { name: "Contact", href: "/contact" },
+    ];
+
+    const pathName = usePathname();
+    return (
+        <View
+            style={{
+                backgroundColor: "white",
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 3 },
+                shadowOpacity: 0.25,
+                shadowRadius: 3,
+                zIndex: 1,
+            }}
+        >
+            <View
+                style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    paddingHorizontal: 100,
+                    height: 80,
+                }}
+            >
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Image
+                        style={{
+                            width: 32,
+                            height: 32,
+                            borderRadius: 16,
+                            marginRight: 20,
+                        }}
+                        source={require("../../../assets/images/adminLogo.png")}
+                    />
+                    <View style={{ marginLeft: 8, flexDirection: "row" }}>
+                        {navigation.map((item, index) => (
+                            <Link href={item.href} key={index}>
+                                <TouchableOpacity
+                                    style={{
+                                        marginRight: 30,
+                                        paddingVertical: 28,
+                                        borderBottomWidth: 2,
+                                        borderBottomColor:
+                                            pathName === item.href ? "#374151" : "transparent",
+                                    }}
+                                    onPress={() => console.log(`Navigating to ${item.href}`)}
+                                >
+                                    <Text
+                                        style={{
+                                            fontSize: 16,
+                                            fontWeight: "600",
+                                            color: pathName === item.href ? "#333" : "#6B7280",
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Text>
+                                </TouchableOpacity>
+                            </Link>
+                        ))}
+                    </View>
+                </View>
+                <PaperMenu
+                    style={{ padding: 8, marginTop: 20 }}
+                    visible={visible}
+                    onDismiss={closeMenu}
+                    anchor={
+                        <Pressable onPress={openMenu}>
+                            <Image
+                                style={{ width: 54, height: 54, borderRadius: 26 }}
+                                source={{
+                                    uri: "https://i.ytimg.com/vi/7KxBpKTIl98/maxresdefault.jpg",
+                                }}
+                            />
+                        </Pressable>
+                    }
+                >
+                    <PaperMenu.Item onPress={() => { }} title="Item 1" />
+                </PaperMenu>
+            </View>
+        </View>
+    )
+}
+
+export default Navbar
