2c6096f6b7f97e7516a2cf2be3f41b643926e57e
tunglamdeptrai <sieucuoitnmax@gmail.com>
2024-03-06 17:23:42 +0700

    add create, detail posts

--- app/web/_layout.tsx
+++ app/web/_layout.tsx
@@ -2,12 +2,15 @@ import { Redirect } from "expo-router";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import { Slot } from "expo-router";
 import { PaperProvider } from "react-native-paper";
+import { NativeBaseProvider } from 'native-base';
 export default function Layout() {
   return (
-    <PaperProvider>
-      <AuthProvider>
-        <Slot />
-      </AuthProvider>
-    </PaperProvider>
+    <NativeBaseProvider>
+      <PaperProvider>
+        <AuthProvider>
+          <Slot />
+        </AuthProvider>
+      </PaperProvider>
+    </NativeBaseProvider>
   );
 }
---
+++ constants/fakeData.js
@@ -0,0 +1,17 @@
+const posts = [{
+    id: '1',
+    title: 'Post Title One',
+    content: 'This is the content of the first post...',
+    date: '2023-01-01',
+    imageUrl: 'https://images.pexels.com/photos/1254736/pexels-photo-1254736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+    id: '2',
+    title: 'Post Title Second',
+    content: 'This is the content of the first post...',
+    date: '2023-01-01',
+    imageUrl: 'https://images.pexels.com/photos/1254736/pexels-photo-1254736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+}
+]
+
+export { posts }
