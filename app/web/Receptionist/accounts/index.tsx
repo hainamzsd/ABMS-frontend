b657b97a67de7e5a55678c7174905beca94934de
haidang <machaidangms@gmail.com>
2024-03-06 15:23:38 +0700

    Added receptionist and CMB account management

--- app/web/Receptionist/accounts/index.tsx
+++ app/web/Receptionist/accounts/index.tsx
@@ -39,7 +39,7 @@ export default function AccountManagement() {
             setError(null); // Clear any previous errors
 
             try {
-                const response = await axios.get(`http://localhost:5108/api/v1/account/get?role=2`, {
+                const response = await axios.get(`http://localhost:5108/api/v1/account/get?role=3`, {
                     timeout:100000
                 });
                 setAccountData(response.data.data); // Set account data
