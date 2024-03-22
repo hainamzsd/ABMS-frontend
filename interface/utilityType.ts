2e052dbc7c9595c83430875cfe393bb4df4a9d12
Nguyễn Ngọc Tùng Lâm <85636591+lam1201nhe@users.noreply.github.com>
2024-03-22 19:57:47 +0700

    crud utility

--- interface/utilityType.ts
+++ interface/utilityType.ts
@@ -1,40 +1,50 @@
 export interface Utility {
-    id: string;
-    name: string;
-    openTime: string;
-    closeTime: string;
-    numberOfSlot: number;
-    pricePerSlot: number;
-    description: string;
-    createUser: string;
-    createTime: string;
-    location:string;
-    modifyUser: string;
-    modifyTime: string;
-    status: number;
-  }
+  id: string;
+  name: string;
+  openTime: string;
+  closeTime: string;
+  numberOfSlot: number;
+  pricePerSlot: number;
+  description: string;
+  createUser: string;
+  createTime: string;
+  location: string;
+  modifyUser: string;
+  modifyTime: string;
+  status: number;
+}
+
+export interface UtilityCreation {
+  name: string;
+  buildingId: string;
+  openTime: string;
+  closeTime: string;
+  numberOfSlot: number;
+  pricePerSlot: number;
+  description: string;
+}
 
 export interface UtilityDetail {
-    id: string;
-    name: string;
-    utilityId: string;
-    createUser: string;
-    createTime: string;
-    modifyUser: string;
-    modifyTime: string;
-    status: number;
+  id: string;
+  name: string;
+  utilityId: string;
+  createUser: string;
+  createTime: string;
+  modifyUser: string;
+  modifyTime: string;
+  status: number;
 }
 
 export interface Reservation {
-  id: string,
-  room_id: string,
-  utility_detail_id: string,
-  slot: number,
-  booking_date: string,
-  number_of_person: string,
-  total_price: number,
-  description: string,
-  status: number,
-  utility: string,
-  utility_detail_name: string
+  id: string;
+  room_id: string;
+  utility_detail_id: string;
+  slot: number;
+  booking_date: string;
+  number_of_person: string;
+  total_price: number;
+  description: string;
+  status: number;
+  utility: string;
+  utility_detail_name: string;
 }
